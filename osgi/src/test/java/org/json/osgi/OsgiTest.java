8449771d91262beeebab7c2b6d4da1f4ff1ce72b
Benoît Guérout <benoit.guerout@ext.lbpoker.com>
2013-12-07 12:10:55 +0100

    Add module to test jongo OSGI bundle
    refs bguerout/jongo/#148

---
+++ osgi/src/test/java/org/json/osgi/OsgiTest.java
@@ -0,0 +1,51 @@
+package org.json.osgi;
+
+import org.jongo.Jongo;
+import org.jongo.query.Query;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.osgi.framework.BundleContext;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.assertNotNull;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class OsgiTest {
+
+    @Inject
+    private BundleContext bundleContext;
+
+    @Configuration
+    public Option[] config() {
+        return new Option[]{
+
+                mavenBundle("org.jongo", "jongo", System.getProperty("jongo.version")),
+                mavenBundle("com.fasterxml.jackson.core", "jackson-core", "2.3.0"),
+                mavenBundle("com.fasterxml.jackson.core", "jackson-databind", "2.3.0"),
+                mavenBundle("com.fasterxml.jackson.core", "jackson-annotations", "2.3.0"),
+                mavenBundle("de.undercouch", "bson4jackson", "2.3.0"),
+                mavenBundle("org.mongodb", "mongo-java-driver", "2.11.0"),
+                junitBundles()
+        };
+    }
+
+    @Test
+    public void canCreateAnJongoInstanceFromOsgiContext() {
+
+        Jongo jongo = new Jongo(null);
+
+        Query query = jongo.createQuery("{}");
+
+        assertNotNull(query);
+    }
+}
